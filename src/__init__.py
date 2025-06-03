15e53cdd14e61908c3783960967af779489b6c00
ekaterinagasukova <kgasukova@gmai.com>
2025-06-04 00:59:41 +0300

    Added new converter

--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
-from .converter import csv_to_txt, txt_to_csv, pdf_to_txt, txt_to_pdf
+from .converter import csv_to_txt, txt_to_csv, pdf_to_txt, txt_to_pdf, csv_to_pdf, pdf_to_csv
 
 __all__ = [
     'csv_to_txt',
