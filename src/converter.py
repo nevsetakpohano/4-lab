76856839811cd771d77991f5ad875d200b62be95
ekaterinagasukova <kgasukova@gmai.com>
2025-06-03 23:39:19 +0300

    Implemented TXT to CSV conversion

--- src/converter.py
+++ src/converter.py
@@ -3,3 +3,8 @@ import pandas as pd
 def csv_to_txt(input_path, output_path):
     df = pd.read_csv(input_path)
     df.to_csv(output_path, sep="\t", index=False)
+
+
+def txt_to_csv(input_path, output_path):
+    df = pd.read_csv(input_path, sep="\t")
+    df.to_csv(output_path, index=False)
